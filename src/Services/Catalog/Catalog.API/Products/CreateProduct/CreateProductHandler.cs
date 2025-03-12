26f6863f26e021457c9acb2ddd85d75d6ac922b6
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-13 00:06:43 +0200

    add create product Command and endpoint add Global using

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Create product
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            //Save product
+
+            //Return result
+            return new CreateProductResult(product.Id);
+        }
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,18 @@
+using Carter;
+using Catalog.API.Products.CreateProduct;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 //Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
+app.MapCarter();
 
 // Configure the HTTP request pipeline.
 
