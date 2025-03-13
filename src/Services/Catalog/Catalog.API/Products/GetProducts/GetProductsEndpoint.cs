6ef0e2e181174a5482e93533b611f6f6c2f7e35e
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-13 16:48:43 +0200

    Add Get Product By id move using libraries to global using class

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Catalog.API.Models;
-
-namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts
 {
     public record GetProductsRequest();
     public record GetProductsResponse(IEnumerable<Product> Products);
