61701b498cca8b4c2165c32a66cb2967d4ec53f7
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-13 17:38:12 +0200

    Add Get products by Category delete product

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    public class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger) 
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Querying products by category: {Category}", request.Category);
+            var data = await session.Query<Product>()
+                .Where(p => p.Category.Contains(request.Category)).ToListAsync(cancellationToken);
+            return new GetProductByCategoryResult(data);
+        }
+    }
+}
