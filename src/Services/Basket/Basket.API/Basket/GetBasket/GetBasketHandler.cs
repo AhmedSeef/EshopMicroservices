f67e7e3b0c43065f32c3d64473d4ec50785066de
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-15 15:10:02 +0200

    Add Example for test get Basket Endpoint edit prog class edit global using

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,6 +1,38 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
-    public class GetBasketHandler
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+    public record GetBasketResult(ShoppingCart Cart);
+    public class GetBasketHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
     {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            var dummyItems = new List<ShoppingCartItem>
+            {
+                new ShoppingCartItem
+                {
+                    Quantity = 1,
+                    Color = "Red",
+                    Price = 10.99m,
+                    ProductId = Guid.NewGuid(),
+                    ProductName = "Product 1"
+                },
+                new ShoppingCartItem
+                {
+                    Quantity = 2,
+                    Color = "Blue",
+                    Price = 20.99m,
+                    ProductId = Guid.NewGuid(),
+                    ProductName = "Product 2"
+                }
+            };
+
+            var dummyCart = new ShoppingCart(query.UserName)
+            {
+                Items = dummyItems,
+            };
+
+            // Return dummy data
+            return await Task.FromResult(new GetBasketResult(dummyCart));
+        }
     }
 }
