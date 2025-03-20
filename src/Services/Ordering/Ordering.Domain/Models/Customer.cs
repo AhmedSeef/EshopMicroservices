1e7ac80d041b9e8e59fbf50ea94758c7d57652ee
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-20 18:04:01 +0200

    Complete domain layer

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,13 +1,14 @@
 ﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.ValueObjects;
 
 namespace Ordering.Domain.Models
 {
-    public class Customer : Entity<Guid>
+    public class Customer : Entity<CustomerId>
     {
         public string Name { get; private set; } = default!;
         public string Email { get; private set; } = default!;
 
-        public static Customer Create(Guid id, string name, string email)
+        public static Customer Create(CustomerId id, string name, string email)
         {
             ArgumentException.ThrowIfNullOrWhiteSpace(name);
             ArgumentException.ThrowIfNullOrWhiteSpace(email);
