53d902e3bafc1dd4f2b7861d6edc82e2cabf619f
AhmedGouda <AhmedGouda@DESKTOP-5ORO327>
2025-03-23 09:49:25 +0200

    Complete Ordering Application

--- src/Services/Ordering/Ordering.API/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -4,3 +4,5 @@ global using MediatR;
 global using BuildingBlocks.Exceptions.Handler;
 global using HealthChecks.UI.Client;
 global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using Ordering.API;
+global using Ordering.Infrastructure;
---
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Ordering.Infrastructure.Interceptors;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices
+            (this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<ApplicationDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseSqlServer(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
